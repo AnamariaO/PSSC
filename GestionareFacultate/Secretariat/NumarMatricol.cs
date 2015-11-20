992d1a1dd6f3215b834683f082fbaea25a7286d2
AnamariaO <onetiuanamaria@gmail.com>
2015-11-20 22:23:17 +0200

    Adaugare diagrame
    Adaugare diagrame de activitati si diagrame ale agregatelor

---
+++ GestionareFacultate/Secretariat/NumarMatricol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using Student.Exceptii;
+
+namespace Student
+{
+    public class NumarMatricol
+    {
+        private string _numar;
+        public string Numar { get { return _numar; } }
+
+        public NumarMatricol(string numar)
+        {
+            Contract.Requires<ArgumentNullException>(numar != null, "text");
+            Contract.Requires<EmptyString>(!string.IsNullOrEmpty(numar), "text");
+            Contract.Requires<ArgumentException>(numar.Length == 6, "Numarul matricol are exact 6 caractere.");
+
+            _numar = numar;
+        }
+
+
+        #region override object
+        public override string ToString()
+        {
+            return Numar;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var nume = (NumarMatricol)obj;
+            return Numar.Equals(nume.Numar);
+        }
+
+        public override int GetHashCode()
+        {
+            return Numar.GetHashCode();
+        }
+        #endregion
+    }
+}
